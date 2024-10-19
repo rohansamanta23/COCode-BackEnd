e048db472bd9c125e30494e5fc26a193498a5bfc
rohan samanta <rohansamanta45@gmail.com>
2024-10-19 20:58:50 +0530

    2nd changes

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/login', (req, res) => {
+    res.send('<h1>User Trying to Login</h1>')
+})
+
+app.get('/shadow', (req, res) => {
+    res.send('<h1>User is in to the Shadows</h1>')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
