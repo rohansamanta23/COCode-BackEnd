9e4fa1e122c653052112272c265bf8d2ff2bb8d4
rohan samanta <rohansamanta45@gmail.com>
2024-10-20 18:24:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import dotenv from 'dotenv';
 import express from "express"
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+
 
 dotenv.config();
+const __dirname = dirname(fileURLToPath(import.meta.url))
 const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(__dirname+"/public/index.html")
 })
 
 app.get('/login', (req, res) => {
